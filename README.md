7d7b69ab7f72cf50e344980f2a23af66c660ffff
Mike Bolsunovskyi <mike@bolsunovskyi.com>
2019-01-03 13:56:28 +0200

    start to implement wav recognizer

---
+++ README.md
@@ -0,0 +1,6 @@
+### Generate asset
+text2wave -eval '(voice_msu_ru_nsh_clunits)' < textfile > wavfile.wav
+echo "включи свет на кухне" | text2wave -eval '(voice_msu_ru_nsh_clunits)' -o kitchen.wav
+
+### Record wav voice to stdout
+rec -c 1 -t wav /dev/stdout rate 16k silence 1 0.1 3% 1 3.0 3% > /tmp/rec.wav
---
+++ wav_recognizer/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/xlab/closer"
+	"github.com/xlab/pocketsphinx-go/sphinx"
+)
+
+var (
+	sampleRate          float64
+	hmm                 string
+	dict                string
+	lm                  string
+	jsgf                string
+	keyPhrase           string
+	debugLevel          int
+	logFile             string
+	nfft                int
+	assistantConfigPath string
+)
+
+type Listener struct {
+	inSpeech   bool
+	uttStarted bool
+	dec        *sphinx.Decoder
+}
+
+func init() {
+	flag.Float64Var(&sampleRate, "sr", 44100, "sample rate")
+	flag.StringVar(&hmm, "hmm", "", "directory containing acoustic model files")
+	flag.StringVar(&dict, "dict", "", "main pronunciation dictionary (lexicon) input file")
+	flag.StringVar(&lm, "lm", "", "word trigram language model input file")
+	flag.StringVar(&jsgf, "jsgf", "", "grammar file")
+	flag.StringVar(&keyPhrase, "keyphrase", "", "keyphrase")
+	flag.StringVar(&logFile, "lf", "/dev/null", "log file")
+	flag.IntVar(&debugLevel, "dl", 0, "debug level")
+	flag.IntVar(&nfft, "nfft", 0, "nfft")
+	flag.StringVar(&assistantConfigPath, "ac", "../config/config.yaml", "assistant config path")
+	flag.Parse()
+
+	if hmm == "" || dict == "" || (lm == "" && jsgf == "") {
+		log.Fatalln("hmm, dict and lm or jsgf must be specified")
+	}
+}
+
+func main() {
+	defer closer.Close()
+	closer.Bind(func() {
+		log.Println("Bye!")
+	})
+
+	cfg := sphinx.NewConfig(
+		sphinx.HMMDirOption(hmm),
+		sphinx.DictFileOption(dict),
+		sphinx.SampleRateOption(float32(sampleRate)),
+		sphinx.DebugOption(debugLevel),
+		sphinx.LogFileOption(logFile),
+	)
+
+	if lm != "" {
+		sphinx.LMFileOption(lm)(cfg)
+	}
+
+	if jsgf != "" {
+		sphinx.UserOption("-jsgf", sphinx.String(jsgf))(cfg)
+	}
+
+	if keyPhrase != "" {
+		sphinx.KeyphraseOption(keyPhrase)
+	}
+
+	if nfft != 0 {
+		sphinx.UserOption("-nfft", nfft)(cfg)
+	}
+
+	log.Println("Loading CMU PhocketSphinx.")
+	log.Println("This may take a while depending on the size of your model.")
+	dec, err := sphinx.NewDecoder(cfg)
+	if err != nil {
+		closer.Fatalln(err)
+	}
+	closer.Bind(func() {
+		dec.Destroy()
+	})
+
+	l := &Listener{
+		dec: dec,
+	}
+
+	if !dec.StartUtt() {
+		closer.Fatalln("[ERR] Sphinx failed to start utterance")
+	}
+	log.Println("Ready..")
+	closer.Hold()
+}
+
+func (l *Listener) report() {
+	hyp, _ := l.dec.Hypothesis()
+	if len(hyp) > 0 {
+		log.Printf("    > hypothesis: %s", hyp)
+		return
+	}
+
+	log.Println("ah, nothing")
+}
